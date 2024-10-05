78e73a6ba598aa9367e86abcbd181d38fefd085a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-10-05 14:26:15 +0200

    Implement and test Ship class

---
+++ src/js/classes/ship/ship.js
@@ -0,0 +1,14 @@
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
---
+++ src/js/classes/ship/ship.test.js
@@ -0,0 +1,28 @@
+import Ship from "./ship";
+
+const LENGTH = 5;
+
+describe("Ship", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(LENGTH);
+  });
+
+  it("creates a ship with proper length", () =>
+    expect(ship.length).toBe(LENGTH));
+
+  it("increases the number of hits", () => {
+    for (let i = 0; i <= 2; i += 1) {
+      expect(ship.hits).toBe(i);
+      ship.hit();
+    }
+  });
+
+  it("checks if the ship is sunk", () => {
+    expect(ship.isSunk()).toBe(false);
+
+    for (let i = 0; i < LENGTH; i += 1) ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
