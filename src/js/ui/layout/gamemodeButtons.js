8efa3f7961c1bbf20bb8363f97cf4c4879e4715a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-10-08 17:14:38 +0200

    Add game mode selection functionality

--- src/js/ui/layout/gamemodeButtons.js
+++ src/js/ui/layout/gamemodeButtons.js
@@ -1,16 +1,24 @@
 import createGamemodeButton from "../components/buttons/gamemodeButton";
 import friendIcon from "../../../assets/icons/friend.svg";
 import botIcon from "../../../assets/icons/bot.svg";
+import gameplayManager from "../gameplayManager";
+import Player from "../../classes/player/player";
+import screenMessages from "../components/messages/screenMessages";
 
+const main = document.querySelector(".main");
 const section = document.querySelector(".gamemode-btns");
 
 export default function createGamemodeSection() {
   const friendBtn = createGamemodeButton("FRIEND", friendIcon, () => {
-    // TODO: Start game with friend
+    main.classList.add("hidden");
+    screenMessages.get("hideScreen").open();
   });
 
   const botBtn = createGamemodeButton("BOT", botIcon, () => {
-    // TODO: Start game with bot
+    const player = new Player(false);
+    const bot = new Player(true);
+
+    gameplayManager.startGame(".main", player, bot);
   });
 
   section.append(friendBtn, botBtn);
